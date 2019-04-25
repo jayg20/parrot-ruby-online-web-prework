82fcc0958ad2c32d84ace8fec493e961d72b3580
James Gomez <james.gomez320@yahoo.com>
2019-04-25 13:35:23 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,4 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(call="Pretty Bird!", phrase="Squawk!")
-  puts "#{call}"
-  return "#{call}"
-end
+def parrot(phrase = "Squawk!")
+  
